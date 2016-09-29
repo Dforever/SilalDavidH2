d8ebc4b0390f1f4094fbcf1f66f37dd063c7eb75
David Silal <dsilal@albany.edu>
2016-09-29 19:25:03 -0400

    VerifiyingXvalue

--- Rec.java
+++ Rec.java
@@ -7,3 +7,18 @@ public static void main(String args[]) {
 	double EValue = 0.0;
 	double finalGuess = 0.0;
 
+	Scanner input = new Scanner(System.in);
+
+		// Will print and let the user know that the Babylonian Method is being used
+		System.out.println("Square Root Finder (Babylonian Method)");
+		while (xValue <= 0) {
+		System.out.print("Please input a proper value for the x: ");
+		xValue = input.nextDouble();
+
+		//This if statement will not let the user enter a number that is lower than 0.
+		if (xValue <= 0) {
+			System.out.println("Cannot input a value lower than 0. Value entered must be greater than 0");
+		}
+
+}
+
