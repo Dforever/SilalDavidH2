a0a13a625384023cce7f3291cb5100b7e0913da1
David Silal <dsilal@albany.edu>
2016-09-29 19:23:51 -0400

    IntializeMain

---
+++ Rec.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Rec {
+public static void main(String args[]) {
+	//These are the intializers for the main class Rec
+	double xValue = 0.0;
+	double EValue = 0.0;
+	double finalGuess = 0.0;
+
